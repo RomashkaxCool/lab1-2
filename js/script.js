66625131ba8d0771c3c36a61f816640326c8d3f8
Maximov Roman <romchukmaximov@gmail.com>
2025-03-18 12:21:39 +0200

    ninth commit

--- js/script.js
+++ js/script.js
@@ -3,26 +3,25 @@ document.addEventListener("DOMContentLoaded", function () {
   const mobileMenu = document.getElementById("mobile-menu");
   const menuLinks = document.querySelectorAll(".mobile-menu ul li a");
 
-  // Відкриття меню при натисканні на бургер
+
   burger.addEventListener("click", function (event) {
-    event.stopPropagation();  // Перешкоджаємо пропагуванню події, щоб не закрило меню при натисканні на бургер
-    burger.classList.toggle("active");  // Перемикаємо клас 'active' для анімації
-    mobileMenu.classList.toggle("active");  // Відкриваємо меню
+    event.stopPropagation();
+    burger.classList.toggle("active");
+    mobileMenu.classList.toggle("active");
   });
 
-  // Закриття меню при натисканні на посилання
   menuLinks.forEach(link => {
     link.addEventListener("click", function () {
-      mobileMenu.classList.remove("active");  // Закриваємо меню
-      burger.classList.remove("active");  // Прибираємо анімацію хрестика
+      mobileMenu.classList.remove("active");
+      burger.classList.remove("active");
     });
   });
 
-  // Закриття меню при натисканні поза меню та бургером
+
   document.addEventListener("click", function (event) {
     if (!mobileMenu.contains(event.target) && !burger.contains(event.target)) {
-      mobileMenu.classList.remove("active");  // Закриваємо меню
-      burger.classList.remove("active");  // Прибираємо анімацію хрестика
+      mobileMenu.classList.remove("active");
+      burger.classList.remove("active");
     }
   });
 });
